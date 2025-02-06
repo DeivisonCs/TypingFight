3a581c716e72f876c19a6a70a27b9fb1b10a563f
Deivison Santos <deivisonddj3@gmail.com>
2025-02-06 12:03:22 -0300

    create forms

--- TypingFight/src/pages/CreateMatchPage/index.tsx
+++ TypingFight/src/pages/CreateMatchPage/index.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./styles.css";
 
 const CreateMatchPage: React.FC = () => {
+    const [isProtected, setIsProtected] = useState(false);
+
+    function changeProtect() {
+        setIsProtected(!isProtected);
+    }
+
     return (
-        <h1>CreateMatchPage</h1>
+        <section>
+            <h1>Criar Partida</h1>
+            <form>
+                <div className="top-input-container">
+                    <div className="input-field-div name-field-div">
+                        <input
+                            id="name-field"
+                            name="name-field"
+                            type="text"
+                            maxLength={30}
+                            placeholder="Nome da Partida"/>
+                        <span className="input-bottom-line"></span>
+                    </div>
+
+                    <div className="input-field-div protected-field-div">
+                        <label className="protected-field-label">Protegida:</label>
+                        <div onClick={() => changeProtect()} className="protected-input-div">
+                            <span className={isProtected? "protect-activated protected-input-changer" : "protect-deactivated protected-input-changer"}></span>
+                        </div>
+                        <span className="protected-field-legend">S/N</span>
+                    </div>
+                </div>
+                
+                { isProtected &&
+                    (<div className="input-field-div password-field-div">
+                        <input 
+                            id="password-field" 
+                            name="password-field" 
+                            type="text" 
+                            maxLength={30}
+                            placeholder="Senha"/>
+                        <span className="input-bottom-line"></span>
+                    </div>)
+                }
+            </form>
+        </section>
     )
 }
 
