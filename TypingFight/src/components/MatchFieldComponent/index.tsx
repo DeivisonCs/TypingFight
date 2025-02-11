1488a707fa96f66a7c223f79b3987dc998ab0c24
Deivison Santos <deivisonddj3@gmail.com>
2025-02-10 22:26:45 -0300

    letter activating

--- TypingFight/src/components/MatchFieldComponent/index.tsx
+++ TypingFight/src/components/MatchFieldComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
 import PointsBarComponent from "../PointsBarComponent";
@@ -10,10 +10,47 @@ interface MatchProps {
 
 const MatchFieldComponent: React.FC<MatchProps> = ({player}) => {
     const [points, setPoints] = useState(0);
+    const [letterIndex, setLetterIndex] = useState(0);
+    const [allWords, setWords] = useState<string[]>([]);
+    const [word, setWord] = useState('Ler');
 
     function updatePoints() {
-        setPoints(points+10);
+        setPoints((prevPoints) => prevPoints+10);
     }
+
+    function sortWord() {
+        const nextWord = Math.floor(Math.random() * allWords.length);
+        setWord(allWords[nextWord]);
+    }
+
+    useEffect(() => {
+        fetch('/words.txt')
+            .then(response => response.text())
+            .then(data => {
+                const wordArray = data.split(',').map(word => word.trim());
+                setWords(wordArray);
+            })
+            .catch((error) => console.error('Erro ao carregar as palavras:', error));
+
+
+        const handleKeyDown = (event: KeyboardEvent) => {  
+            if(event.key.toLowerCase() == word[letterIndex].toLowerCase()){
+                setLetterIndex((prevIndex) => prevIndex + 1);
+            }
+
+        };
+
+        if(letterIndex >= word.length){
+            sortWord();
+            setLetterIndex(0);
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [letterIndex]);
 
     return (
         <section 
@@ -25,7 +62,7 @@ const MatchFieldComponent: React.FC<MatchProps> = ({player}) => {
 
                 {player &&
                     <div>
-                        <WordComponent word="teste" typed={1}/>
+                        <WordComponent word={word} typed={letterIndex}/>
                     </div>
                 }
         </section>
